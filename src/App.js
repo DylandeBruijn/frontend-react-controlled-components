1b087e83a9a842904b27c708aa468a9ebaeb5ede
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-05-22 15:49:47 +0200

    Solution for assignement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,84 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./styles/App.scss";
 
 function App() {
-  return (
-    <div>
-      Maak hier jouw formulier!
-    </div>
-  );
+	const [form, setForm] = React.useState({
+		name: "",
+		age: "",
+		found: "",
+		comment: "",
+	});
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		setForm({
+			...form,
+			[e.target.name]: value,
+		});
+	};
+
+	const submitForm = (e) => {
+		e.preventDefault();
+		console.log(form);
+	};
+
+	return (
+		<main>
+			<form onSubmit={submitForm}>
+				<fieldset>
+					<legend>Gegevens</legend>
+					<label htmlFor="name">
+						Naam:
+						<input
+							type="text"
+							name="name"
+							id="name"
+							onChange={handleChange}
+							value={form.name}
+						/>
+					</label>
+					<label htmlFor="age">
+						Leeftijd:
+						<input
+							type="number"
+							name="age"
+							id="age"
+							onChange={handleChange}
+							value={form.age}
+						/>
+					</label>
+				</fieldset>
+				<fieldset>
+					<legend>Jouw review</legend>
+					<label htmlFor="found">
+						Hoe heb je dit recept gevonden?
+						<select
+							name="found"
+							id="found"
+							onChange={handleChange}
+							value={form.found}
+						>
+							<option value="Google">Google</option>
+							<option value="Vriend">Vriend</option>
+							<option value="Advertentie">Advertentie</option>
+							<option value="Anders">Anders</option>
+						</select>
+					</label>
+					<label htmlFor="comment">
+						Opmerkingen
+						<textarea
+							name="comment"
+							id="comment"
+							rows="5"
+							onChange={handleChange}
+							value={form.comment}
+						></textarea>
+					</label>
+					<button type="submit">Versturen</button>
+				</fieldset>
+			</form>
+		</main>
+	);
 }
 
 export default App;
